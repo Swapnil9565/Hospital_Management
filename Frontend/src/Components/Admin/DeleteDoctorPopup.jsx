fe4d7dde0f87035e62a3d474786b2a93dbbd5e13
Swapnil Mohite <swapnilmohite9565@gmail.com>
2025-02-21 11:50:17 +0530

    .

--- Frontend/src/Components/Admin/DeleteDoctorPopup.jsx
+++ Frontend/src/Components/Admin/DeleteDoctorPopup.jsx
@@ -4,7 +4,8 @@ import {toast} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css";
 const DeleteDoctorPopup = ({setIsOpenDeletePopup,doctorId}) => {
     
- const handleDeleteDoctor=async(doctorId)=>{
+ const handleDeleteDoctor=async()=>{
+    if (!doctorId) return;
     try {
         const res=await axios.delete(`https://hospital-management-99yz.onrender.com/api/admin/deleteDoctor/${doctorId}`,{
             headers:{
@@ -24,7 +25,7 @@ const DeleteDoctorPopup = ({setIsOpenDeletePopup,doctorId}) => {
              )
         }
     } catch (error) {
-        toast.error("Something went wrong while deleting doctor",{
+        toast.error( error.response?.data?.message || "Something went wrong while deleting doctor",{
             position: "top-center",
             autoClose: 3000,
             hideProgressBar: false,
@@ -40,7 +41,7 @@ const DeleteDoctorPopup = ({setIsOpenDeletePopup,doctorId}) => {
     <div className='w-60 h-32 bg-slate-900 text-white p-5 rounded-md'>
         <p className='text-lg text-center mb-5'>Are you sure you want to delete this doctor?</p>
         <div className="flex justify-between items-center gap-5 px-5">
-            <button className='text-blue-400' onClick={()=>handleDeleteDoctor(doctorId)} >Confirm</button>
+            <button className='text-blue-400' onClick={handleDeleteDoctor} >Confirm</button>
             <p>|</p>
             <button onClick={()=>setIsOpenDeletePopup(false)}>Cancel</button>
         </div>
