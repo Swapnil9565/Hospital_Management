980b185814d503b70911242de9868105b0d55945
Swapnil Mohite <swapnilmohite9565@gmail.com>
2025-03-08 10:36:57 +0530

    Feat:Edit profile api

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt=require("bcryptjs")
+const bcrypt=require("bcrypt")
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const appointmentModel = require("../models/appointmentModel");
